1cd332f51789374fa966441e76aa16b2c1e9e9f0
PitaKibunja <pitakibunja@gmail.com>
2023-09-09 08:52:21 -0700

    fixed the two errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-/*
- * main - Main Entry of the program
+/**
+ * main - Entry of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
-	
+	int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
