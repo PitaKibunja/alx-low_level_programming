bbf90e92ad43999db4483c3f951646cff1bd0560
PitaKibunja <pitakibunja@gmail.com>
2023-09-14 14:38:52 -0700

    added a function with recursion

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer to the console
+ *
+ * @n: integer to be printed
+ */
+void print_number(int a)
+{
+	unsigned int a1;
+
+	if (a)
+	{
+		a1 = -a;
+		_putchar ('-');
+	} else
+	{
+		a1 = a;
+	}
+
+	if (a1 / 10)
+	{
+		print_number(a1 / 10);
+	}
+
+	_putchar ((a1 % 10) + '0');
+}
